58a22649e8a8b7085433a7fb1d99423e976f58bd
Nguyễn Văn Bảo Linh <you@example.com>
2022-06-16 23:03:22 +0700

    upload

---
+++ src/java/Model/User.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Baoli
+ */
+public class User {
+    private String username,password,avatar,displayname,dob,email,roleID;
+    
+    public User() {
+    }
+    
+    public User(String username, String password, String avatar, String displayname, String dob, String email, String roleID) {
+        this.username = username;
+        this.password = password;
+        this.avatar = avatar;
+        this.displayname = displayname;
+        this.dob = dob;
+        this.email = email;
+        this.roleID = roleID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(String roleID) {
+        this.roleID = roleID;
+    }
+    
+}
